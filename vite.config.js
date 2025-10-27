00f089039181798d1dcc50cf2a2cff4429c67158
Kaushik Sambamurthy <kaushik.s02@gmail.com>
2025-10-27 18:08:16 -0400

    Add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    base: process.env.NODE_ENV === 'production' ? '/kindle-clippings-reader/' : '/',
     server: {
         host: true,
         port: 5173,
